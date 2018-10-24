627c86b55a13e96e1d394cd309833a56a7f1223a
Jerome Devost <jdevost@coveo.com>
2018-10-24 12:31:46 -0400

    support --init and -init, add validation on folder name for init

--- src/main.js
+++ src/main.js
@@ -1,22 +1,20 @@
 const fs = require('fs');
 
-const {createDefault, getConfig} = require('./config');
+const { createDefault, getConfig } = require('./config');
 const deploy = require('./deploy');
 
 let ARGS = {};
 
-let parseArgs = ()=> {
+let parseArgs = () => {
   const args = process.argv.slice(2);
 
   while (args.length) {
     let arg = args.shift().trim();
-    if (arg === '-init') {
+    if (/^-+init$/.test(arg)) {
       ARGS.doInit = true;
-    }
-    else if ( /^-/.test(arg)) {
+    } else if (/^-/.test(arg)) {
       console.log('Do not understand this argument: ', arg);
-    }
-    else {
+    } else {
       ARGS.folder = arg;
     }
   }
@@ -24,23 +22,24 @@ let parseArgs = ()=> {
 
 parseArgs();
 if (ARGS.doInit) {
-  console.log(`Create folder "${ARGS.folder}" and initialize page`);
-  createDefault(ARGS.folder);
-}
-else {
+  if (ARGS.folder) {
+    console.log(`Create folder "${ARGS.folder}" and initialize page.`);
+    createDefault(ARGS.folder);
+  } else {
+    console.log(`Missing folder name.`);
+  }
+} else {
   // validate FOLDER first
   if (!ARGS.folder) {
     ARGS.folder = '.';
   }
-  if ( !(fs.existsSync(ARGS.folder + '/page.html') && fs.existsSync(ARGS.folder + '/page.scss') && fs.existsSync(ARGS.folder + '/page.js')) ) {
+  if (!(fs.existsSync(ARGS.folder + '/page.html') && fs.existsSync(ARGS.folder + '/page.scss') && fs.existsSync(ARGS.folder + '/page.js'))) {
     console.log('Not a valid folder. Missing page.html, page.scss, or page.js.');
     process.exit();
   }
   //
   console.log(`Deploying search page from ${ARGS.folder}`);
-  getConfig(ARGS.folder).then(config=>{
+  getConfig(ARGS.folder).then(config => {
     deploy(ARGS.folder, config);
   });
 }
-
-
